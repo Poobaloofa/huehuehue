468246361c18c0cebc7f71c621f185bb895dd6d1
Poobaloofa <julian.ivaldi@gmail.com>
2017-06-21 21:05:19 -0400

    Delete hue.js

--- hue.js
+++
@@ -1,8 +0,0 @@
-function hueHue(){
-document.getElementById("huehue").style.visibility="visible";
-document.getElementById("gibemoni").style.visibility="hidden"
-}
-function iReportU(){
-document.getElementById("gibemoni").style.visibility="visible";
-document.getElementById("huehue").style.visibility="hidden"
-}
