9095d0130a1d57b7cc8eba257a3c4f8a614f130d
Poobaloofa <julian.ivaldi@gmail.com>
2017-06-21 21:03:24 -0400

    Create hue.js

---
+++ hue.js
@@ -0,0 +1,8 @@
+function hueHue(){
+document.getElementById("huehue").style.visibility="visible";
+document.getElementById("gibemoni").style.visibility="hidden"
+}
+function iReportU(){
+document.getElementById("gibemoni").style.visibility="visible";
+document.getElementById("huehue").style.visibility="hidden"
+}
